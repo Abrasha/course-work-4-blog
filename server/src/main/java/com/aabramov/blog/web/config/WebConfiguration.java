ad6ccd9839c8057a6bc3d5e6769583f70e1b06d7
Andrii <aabrasha@gmail.com>
2017-02-26 22:36:36 +0200

    - add CORS support

--- server/src/main/java/com/aabramov/blog/web/config/WebConfiguration.java
+++ server/src/main/java/com/aabramov/blog/web/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.aabramov.blog.web.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -16,5 +17,9 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         return new ModelMapper();
     }
     
-    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/blog/api/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
